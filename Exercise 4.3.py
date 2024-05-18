0b0023e850f79a1da2adf0d9aa3c123f35268c27
VylaDonis <139762674+VylaDonis@users.noreply.github.com>
2024-05-18 17:32:45 +0200

    Add files via upload

---
+++ Exercise 4.3.py
@@ -0,0 +1,11 @@
+my_list = ['h', 'e', 'l', 'l', 'o']
+if len(my_list) != 5:
+        print("This list doesn’t have the right size")
+else:
+        print("The string is: " + ''.join(my_list))
+
+my_list2 = ['h', 'e', 'y']
+if len(my_list2) != 5:
+        print("This list doesn’t have the right size")
+else:
+        print("The string is: " + ''.join(my_list2))
