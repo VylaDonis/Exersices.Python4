fa1c2b8c62b5e3c480a15ea2cf8eb07ef2c3c144
VylaDonis <139762674+VylaDonis@users.noreply.github.com>
2024-05-18 17:32:31 +0200

    Add files via upload

---
+++ Exersice 4.2.py
@@ -0,0 +1,12 @@
+my_list = [6,2,7,4,3]
+largest = max(my_list)
+smallest = min(my_list)
+my_list.remove(largest)
+my_list.remove(smallest)
+print(my_list)
+my_list1 = [6,6,3,2,4,5,5]
+largest = max(my_list1)
+smallest = min(my_list1)
+my_list1.remove(largest)
+my_list1.remove(smallest)
+print(my_list1)
